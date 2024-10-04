420254663d9dbd1e03f0068fce77664c1c98449e
Calel Costa <calel.costa@gmail.com>
2024-10-04 10:14:40 -0300

    Adjst query filter

--- src/main/java/desafio/catalagosabio/infra/repository/BookRepository.java
+++ src/main/java/desafio/catalagosabio/infra/repository/BookRepository.java
@@ -14,9 +14,9 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query(value = "select b from Book b where b.genre = :genre")
+    @Query(value = "select b from Book b where LOWER(b.genre) LIKE LOWER(CONCAT('%', REPLACE(:genre, ' ', '%'), '%'))\n")
     List<Book> findByGenre(@Param("genre") String genre);
 
-    @Query(value = "select b from Book b where b.author = :author")
+    @Query(value = "select b from Book b where LOWER(b.author) LIKE LOWER(CONCAT('%', REPLACE(:author, ' ', '%'), '%'))\n")
     List<Book> findByAuthor(@Param("author") String author);
 }
